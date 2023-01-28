d20f09dc5e8d7072434728b78d88a4731bbcc962
gxy1113 <gxy981113@gmail.com>
2023-01-27 21:22:52 -0500

    test

--- Functions.py
+++ Functions.py
@@ -289,7 +289,10 @@ def allow_edge(graph, edge):
     so = same_origin(from_url, to_url)
 
     # TODO: More general solutions ? e.g regex patterns, counts etc.
-    blacklisted_terms = []
+    blacklisted_terms = ['profile', 'update', 'password', 'maintenance', 'customize.php', 'xml', 'json', 'rss', 'Tsv', 'search.php',
+                    'user/1/edit', 'user/2/edit', 'user/3/edit', 'CorePluginsAdmin', 'UsersManager', 'users.php', 'update', 'page=config', 
+                    'people', 'roles', 'authentication', 'usermanager', 'user/user', '=acl', 'page=extension', 'mode=cookie', "edituser", 
+                    'help', 'r=user', 'r=admin', 'viewpmsg', 'logout']
     # For example
     # blacklisted_terms.extend( ["logout"] )
     if blacklisted_terms:
