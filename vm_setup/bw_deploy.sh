d702d3b2916e064192856a87d466e7c0ad8331f1
gxy1113 <gxy981113@gmail.com>
2023-02-22 15:35:04 -0500

    test

--- vm_setup/bw_deploy.sh
+++ vm_setup/bw_deploy.sh
@@ -1,6 +1,8 @@
-echo "Hostname of the AuthZee VM:"
+echo "Hostname of the BW VM:"
 read bwID
-echo "Target Web application: "
+echo "Password of the BW VM: "
+read password
+echo "Target Web application URL: "
 read webapp
-sshpass -p "vmuser" scp -o StrictHostKeyChecking=no bw_run.sh vmuser@$bwID.csl.toronto.edu:/home/vmuser/wk_dir/
-sshpass -p "vmuser" ssh -o StrictHostKeyChecking=no -t vmuser@$bwID.csl.toronto.edu "/home/vmuser/wk_dir/bw_run.sh $webapp"
+sshpass -p $password scp -o StrictHostKeyChecking=no bw_run.sh vmuser@$bwID.csl.toronto.edu:/home/vmuser/wk_dir/
+sshpass -p $password ssh -o StrictHostKeyChecking=no -t vmuser@$bwID.csl.toronto.edu "/home/vmuser/wk_dir/bw_run.sh $webapp"
