d81bbbc97bd340f3fc3fac8c31e3e776038549e5
gxy1113 <gxy981113@gmail.com>
2023-02-22 16:19:41 -0500

    test

--- vm_setup/bw_run.sh
+++ vm_setup/bw_run.sh
@@ -9,6 +9,8 @@ mkdir data/
 mkdir form_files/dynamic
 git stash
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull
+date +%s > start_time
 screen -dmS pts-bw python3 crawl.py --url $1 --crawler
 sleep 15
 screen -ls
+cat start_time
