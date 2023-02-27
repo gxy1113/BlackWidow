91b0f2c14a25f1baec0fb18cbe07158e8d266f2f
gxy1113 <gxy981113@gmail.com>
2023-02-27 11:06:12 -0500

    test

--- parse_log.py
+++ parse_log.py
@@ -5,8 +5,10 @@ form_url = []
 form_html_url = []
 url_log = "data/url_log.txt"
 html_url_log = "data/html_url_log.txt"
+all_form_log = "data/all_forms.txt"
 event_edge = "event_edge.txt"
 appname = sys.argv[1]
+mode = sys.argv[2]
 
 def load_file(folder, name):
     try:
@@ -30,6 +32,11 @@ def read_log(path=""):
         with open(path) as f:
             for line in f:
                 line_str = line.strip()
+                if "xss" in line_str:
+                    continue
+                if "jaekpot" in line_str:
+                    continue
+                line_str = parse_url(line_str)
                 result = 0
                 for item in log:
                     if item == line_str:
@@ -57,12 +64,58 @@ def read_and_convert(path=""):
         f.close()
     return log
 
+def remove_milisecond(start_time=""):
+    start_time = start_time.split('.')[0]
+    return int(start_time)
 
-form_url = read_log(url_log)
-form_html_url = read_log(html_url_log)
-success_forms = {"html": form_html_url, "url": form_url}
-event_dict = read_and_convert("data/" + event_edge)
-json_object = json.dumps(success_forms, indent=4)
-write_file("data/", "event_edge.json", event_dict)
-with open("data/" + appname + ".json", "w") as outfile:
-    outfile.write(json_object)
+def parse_edge(event_edge = {}):
+    event_edge["start_time"] = remove_milisecond(event_edge["start_time"])
+    event_edge["end_time"] = remove_milisecond(event_edge["end_time"])
+    event_edge['interval'] = event_edge["end_time"] - event_edge["start_time"]
+    event_edge["new_eles"] = int(event_edge["new_eles"])
+    return event_edge
+
+def collect_data(event_list = list()):
+    cnt = 0
+    cumulated_time = 0
+    for event_edge in event_list:
+        if event_edge["new_eles"] == 0:
+            cnt = cnt + 1
+            cumulated_time = cumulated_time + event_edge["interval"]
+    return[len(event_list), cnt, cumulated_time / 60]
+
+def parse_url(url=""):
+    url = url.replace("http://", "")
+    url_components = url.split("/")
+    for component in url_components:
+        if component.isnumeric():
+            component = "1"
+    url = "http://" + '/'.join(url_components)
+    return url
+
+
+if __name__ == '__main__':
+    form_url = read_log(url_log)
+    form_html_url = read_log(html_url_log)
+    all_form = read_log(all_form_log)
+    success_forms = {"html": form_html_url, "url": form_url}
+    event_dict = read_and_convert("data/" + event_edge)
+    event_list = list()
+    for event_edge in event_dict:
+        event_edge = parse_edge(event_edge)
+    json_object = json.dumps(success_forms, indent=4)
+    result = collect_data(event_dict)
+    print(result)
+    write_file("data/", "event_edge.json", event_dict)
+    write_file("data/", "all_form.json", all_form)
+    success_forms = success_forms[mode]
+    failed_forms = list()
+    for url in all_form:
+        if url in success_forms:
+            continue
+        failed_forms.append(url)
+    classified_forms = {"success": success_forms, "failed": failed_forms}
+    write_file("data/", "classified_form.json", classified_forms)
+    with open("data/" + appname + ".json", "w") as outfile:
+        outfile.write(json_object)
+    
